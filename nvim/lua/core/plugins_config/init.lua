2816bf7c109df253f972dd8ccbfc3ec5c56dd1d8
Joseph Hanger <48699911+joerhang1365@users.noreply.github.com>
2023-11-26 14:18:11 -0600

    Add files via upload

---
+++ nvim/lua/core/plugins_config/init.lua
@@ -0,0 +1,5 @@
+require("core.plugins_config.onedark")
+require("core.plugins_config.lualine")
+require("core.plugins_config.completions")
+require("core.plugins_config.lsp")
+require("core.plugins_config.telescope")
