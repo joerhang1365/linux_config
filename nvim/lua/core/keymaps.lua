b83d92f4253c257f63164ddc6c67285d1a19ae53
Joseph Hanger <48699911+joerhang1365@users.noreply.github.com>
2024-09-02 17:41:44 -0500

    Add files via upload

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -1,21 +1,7 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
-vim.opt.backspace = '2'
-vim.opt.showcmd = true
-vim.opt.laststatus = 2
-vim.opt.autowrite = true
-vim.opt.cursorline = true
-vim.opt.autoread = true
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.shiftround = true
-vim.opt.expandtab = true
-vim.opt.number = true
-
 vim.keymap.set('n', '<space>', ':')
-vim.keymap.set('n', '<c-f>', ':Files<cr>')
-vim.keymap.set('n', '<c-t>', ':FloatermNew<cr>')
 vim.keymap.set('i', '<c-x>', '<esc>')
 vim.keymap.set('i', '\'', '\'\'<left>')
 vim.keymap.set('i', '\"', '\"\"<left>')
